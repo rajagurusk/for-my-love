11e451a1b062cee1f4a4d498ae5cc78b5d33cedd
rajagurusk <rajagurusivakumar1@example.com>
2026-02-05 18:19:40 +0530

    nithi

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
       {!yesClicked ? (
         <>
           <img src={myGif} alt="GIF" className="center-image" />
-
-          <h2 className="text">Will u be my valentine?</h2>
+<h2 className="text">Hey Nithiyasri!</h2>
+          <h3 className="text">Will u be my valentine?</h3>
 
           <div className="button-area">
             <button className="btn yes" onClick={handleYesClick}>
